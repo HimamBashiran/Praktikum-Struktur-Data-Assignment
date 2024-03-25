ae01b872d2398799a4b464b5aa3aa1019d8333c5
himambashiran <142086470+HimamBashiran@users.noreply.github.com>
2024-03-25 15:37:59 +0700

    Upload MODUL 2

--- MODUL-2-ARRAY/unguided3.cpp
+++ MODUL-2-ARRAY/unguided3.cpp
@@ -48,9 +48,32 @@ int main() {
     cout << "Nilai maksimum adalah " << maks << " berada di Array ke-" << lokasiMax << endl;
     cout << "Nilai minimum adalah " << min << " berada di Array ke-" << lokasiMin << endl;
 
-    // Menghitung rata-rata
-    rata = jumlah / a;
-    cout << "Nilai rata-ratanya adalah " << rata << endl;
+    // Menampilkan menu
+    cout << "Menu:\n";
+    cout << "1. Maksimum\n";
+    cout << "2. Minimum\n";
+    cout << "3. Rata-rata\n";
+
+    // Pilihan operasi
+    cout << "Pilih operasi: ";
+    int pilihan;
+    cin >> pilihan;
+
+    switch (pilihan) {
+        case 1:
+            cout << "Nilai maksimum adalah " << maks << " berada di Array ke-" << lokasiMax << endl;
+            break;
+        case 2:
+            cout << "Nilai minimum adalah " << min << " berada di Array ke-" << lokasiMin << endl;
+            break;
+        case 3:
+            // Menghitung rata-rata
+            rata = jumlah / a;
+            cout << "Nilai rata-ratanya adalah " << rata << endl;
+            break;
+        default:
+            cout << "Pilihan tidak valid." << endl;
+    }
 
     return 0;
 }
